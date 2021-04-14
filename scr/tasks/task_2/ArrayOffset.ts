e7157a4e63a230ffa3ee3c2a68c263c0d0a7a90e
VolodymyrVlasov <volodymyr.vlasov@gmail.com>
2021-04-14 20:25:29 +0300

    add render html task part method

--- scr/tasks/task_2/ArrayOffset.ts
+++ scr/tasks/task_2/ArrayOffset.ts
@@ -2,6 +2,7 @@ import {Lang, TaskType} from "../../webapp/Enums.js";
 import {AbstractTask} from "../../webapp/AbstractTask.js";
 
 export class ArrayOffset extends AbstractTask{
+
     static  moveArray() {
         let arr: any[] = ['1', '2', '3', '4', '5', 6, '7', 8, '9', '10']    // arr = [8, 9, 10, 1, 2 , 3, 4, 5, 6, 7]
         let offset: number = 3
@@ -20,10 +21,11 @@ export class ArrayOffset extends AbstractTask{
         console.log('array after move ' + arr)
     }
 
-    description: string = "";
-    title: string = "";
-    language: Lang = Lang.TS;
-    type: TaskType = TaskType.ARRAY_MOVER;
+    title: string = "Array mover"
+    description: string = "script that move array element for 3 step in right side"
+    language: Lang = Lang.TS
+    type: TaskType = TaskType.ARRAY_MOVER
+    htmlPart: boolean = false;
 
     renderContainer(): void {
         console.log('Test array offset')
--- scr/webapp/AbstractTask.ts
+++ scr/webapp/AbstractTask.ts
@@ -2,11 +2,12 @@ import {Lang, TaskType} from "./Enums.js";
 import {taskList} from "../index.js";
 
 export abstract class AbstractTask {
-    abstract description: string;
-    abstract language: Lang;
-    abstract title: string;
-    abstract type: TaskType;
-    abstract renderContainer(): void;
+    abstract description: string
+    abstract language: Lang
+    abstract title: string
+    abstract type: TaskType
+    abstract htmlPart: boolean
+    abstract renderContainer(): void
 
     constructor(type: TaskType) {
         taskList.set(type, this)
