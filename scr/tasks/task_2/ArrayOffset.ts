dee0d718453619ed87f67672ddf232bec3aa254e
VolodymyrVlasov <volodymyr.vlasov@gmail.com>
2021-04-14 20:26:48 +0300

    add render html task part method

--- scr/tasks/task_2/ArrayOffset.ts
+++ scr/tasks/task_2/ArrayOffset.ts
@@ -2,6 +2,29 @@ import {Lang, TaskType} from "../../webapp/Enums.js";
 import {AbstractTask} from "../../webapp/AbstractTask.js";
 
 export class ArrayOffset extends AbstractTask{
+    title: string = "Array mover"
+    description: string = "script that move array element for 3 step in right side"
+    language: Lang = Lang.TS
+    type: TaskType = TaskType.ARRAY_MOVER
+    htmlPart: boolean = false;
+
+    static showHtml(htmlPart: boolean) {
+        if (htmlPart) {
+            let rootCnt = document?.getElementById('root_cnt')
+            if (rootCnt != null) {
+                //append inner content here
+
+            }
+        } else {
+            alert('Please, open colsole to view result')
+        }
+    }
+
+    renderContainer(): void {
+        console.log('Test array offset')
+        ArrayOffset.moveArray()
+        ArrayOffset.showHtml(this.htmlPart);
+    }
 
     static  moveArray() {
         let arr: any[] = ['1', '2', '3', '4', '5', 6, '7', 8, '9', '10']    // arr = [8, 9, 10, 1, 2 , 3, 4, 5, 6, 7]
@@ -19,17 +42,6 @@ export class ArrayOffset extends AbstractTask{
         }
 
         console.log('array after move ' + arr)
-    }
-
-    title: string = "Array mover"
-    description: string = "script that move array element for 3 step in right side"
-    language: Lang = Lang.TS
-    type: TaskType = TaskType.ARRAY_MOVER
-    htmlPart: boolean = false;
-
-    renderContainer(): void {
-        console.log('Test array offset')
-        ArrayOffset.moveArray()
     }
 }
 
