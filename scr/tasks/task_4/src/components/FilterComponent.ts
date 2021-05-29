2f2c8513f80a6316eec3f73f8fee0e6ada6158f3
VolodymyrVlasov <volodymyr.vlasov@gmail.com>
2021-05-29 12:35:08 +0300

    add map for filter

--- scr/tasks/task_4/src/components/FilterComponent.ts
+++ scr/tasks/task_4/src/components/FilterComponent.ts
@@ -1,4 +1,4 @@
-import {Color, Os, Memory} from "../types/index.js";
+import {Color, Memory, Os} from "../types/index.js";
 import {FilterItem, IFilterItem} from "./FilterItem.js";
 
 export class FilterComponent {
@@ -6,12 +6,27 @@ export class FilterComponent {
     filterBtn: HTMLButtonElement
     isFilterVisible: boolean = false
     private filterData: Map<Object, IFilterItem>
+    private filterPriceCard: FilterItem
+    private filterValues: Map<String, Array<any>>
+    private filterColors: Array<string>
+    private filterOs: Array<string>
+    private filterMemory: Array<string>
+    private filterPrice: Array<number>
+
+
 
     constructor(rootCntId: string, filterBtnId: string) {
+        this.filterValues = new Map<String, any>()
+        this.filterColors = new Array<string>()
+        this.filterMemory = new Array<string>()
+        this.filterOs = new Array<string>()
+        this.filterPrice = new Array<number>()
         this.filterData = new Map()
         this.addFilterData(Color, 'Color')
         this.addFilterData(Os, 'Os')
         this.addFilterData(Memory, 'Memory')
+
+        this.filterPriceCard = new FilterItem(null, "Title")
 
         this.rootCnt = <HTMLDivElement>document.getElementById(rootCntId)
         this.filterBtn = <HTMLButtonElement>document.getElementById(filterBtnId)
@@ -26,39 +41,10 @@ export class FilterComponent {
 
     }
 
-    private renderPriceFilter(): HTMLDivElement {
-        let filterCnt = <HTMLDivElement>document.createElement('div')
-        filterCnt.innerHTML = `
-                             <div class="product_cnt_filter">
-                        <div class="filter_card">
-
-                            <div class="filter_card_header">
-                                <span class="filter_card_header_title">Price</span>
-                                <div id="filter_item_btn" class="filter_card_header_btn" onclick="collapseItemOnClick('filer_card_1')"></div>
-                            </div>
-
-                            <div id="filer_card_1" class="filter_card_content">
-                                <div class="filter_input_item">
-                                    <label class="filter_input_item_title" for="price_from">From</label>
-                                    <input class="filter_input_item_input" id="price_from" type="text" placeholder="350">
-                                </div>
-                                <div class="filter_input_item">
-                                    <label class="filter_input_item_title" for="price_to">To</label>
-                                    <input class="filter_input_item_input" id="price_to" type="text" placeholder="1350">
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
-            `
-        return filterCnt
-    }
-
     private showFilter(): void {
         if (!this.isFilterVisible) {
             let filtersCnt = <HTMLDivElement>document.createElement('div')
-            filtersCnt.append(this.renderPriceFilter())
-
+            filtersCnt.append(this.filterPriceCard.renderPriceFilter())
             this.filterData.forEach((e) => {
                 filtersCnt.append(e.render())
             })
@@ -80,8 +66,27 @@ export class FilterComponent {
     }
 
     private filter(event: Event) {
-        if (event.target instanceof HTMLInputElement && event.target.type == 'checkbox')
-            console.log(event.target.checked)
+        if (event.target instanceof HTMLInputElement && event.target.type == 'checkbox') {
+            if (event.target.parentNode?.parentNode?.parentNode?.id == 'color') {
+                this.filterColors.push(Color[event.target.id as keyof typeof Color])
+                this.filterValues.set('color', this.filterColors)
+            } else if (event.target.parentNode?.parentNode?.parentNode?.id == 'os'){
+                this.filterOs.push(Os[event.target.id as keyof typeof Os])
+                this.filterValues.set('os', this.filterColors)
+            }
+            else if (event.target.parentNode?.parentNode?.parentNode?.id == 'memory'){
+                this.filterMemory.push(Memory[event.target.id as keyof typeof Memory])
+                this.filterValues.set('storage', this.filterMemory)
+            }
 
+        } else if (event.target instanceof HTMLInputElement && event.target.type == 'text') {
+            if (event.target.id === 'price_from') {
+                this.filterPrice[0] = Number(event.target.value)
+            } else if (event.target.id === 'price_to') {
+                this.filterPrice[1] = Number(event.target.value)
+            }
+            this.filterValues.set('price', this.filterPrice)
+        }
+        console.log(this.filterValues)
     }
 }
--- scr/tasks/task_4/src/components/FilterItem.ts
+++ scr/tasks/task_4/src/components/FilterItem.ts
@@ -3,27 +3,54 @@ export interface IFilterItem {
 }
 
 export class FilterItem implements IFilterItem {
-    private readonly obj: Object
+    private readonly obj: Object | null
     private readonly filterTitle: string
     private filterValues: Array<any>
+    private headerFilter: HTMLDivElement
 
-    constructor(object: Object, filterTitle: string) {
+    constructor(object: Object | null, filterTitle: string) {
         this.obj = object
         this.filterTitle = filterTitle
         this.filterValues = new Array<any>()
+        this.headerFilter = document.createElement('div')
+    }
+
+    public renderPriceFilter(): HTMLElement {
+        let filterCard = <HTMLElement>document.createElement('div')
+        filterCard.className = 'filter_card'
+        filterCard.append(this.addHeader())
+
+        let filterContent = document.createElement('div')
+        filterContent.className = 'filter_card_content'
+        filterContent.id = 'filer_card_1'
+        filterContent.innerHTML = `
+                <div class="filter_input_item">
+                    <label class="filter_input_item_title" for="price_from">From</label>
+                    <input class="filter_input_item_input" id="price_from" type="text" placeholder="350">
+                </div>
+                <div class="filter_input_item">
+                    <label class="filter_input_item_title" for="price_to">To</label>
+                    <input class="filter_input_item_input" id="price_to" type="text" placeholder="1350">
+                </div>
+            `
+
+        filterCard.getElementsByClassName('filter_card_header_btn')[0]
+            .addEventListener('click', (e) => {
+                this.collapseFilter(filterContent.id)
+            })
+        filterCard.append(filterContent)
+        return filterCard
     }
 
     public render(): HTMLElement {
+        if (this.obj == null)
+            throw new Error("filter object is null")
         let o: Object = this.obj
         let rawHTML: HTMLDivElement = document.createElement('div')
         rawHTML.className = 'filter_card'
-        rawHTML.innerHTML = `
-                <div class="filter_card_header">
-                    <span class="filter_card_header_title">${this.filterTitle}</span>
-                    <div id="filter_item_btn" class="filter_card_header_btn"></div>
-                </div>`
+        rawHTML.append(this.addHeader())
 
-        let innerItems: HTMLLIElement[] = Object.keys(this.obj).map((value) => {
+        let innerItems: HTMLLIElement[] = Object.keys(o).map((value) => {
             let rawLi = <HTMLLIElement>document.createElement('li')
             rawLi.innerHTML = ` <div>
                                  <input class="filter_input_item_inut" type="checkbox" id="${value}">
@@ -34,7 +61,6 @@ export class FilterItem implements IFilterItem {
         })
         let filterContent = <HTMLDivElement>document.createElement('div')
         filterContent.id = this.filterTitle.toLocaleLowerCase()
-
         innerItems.forEach((value: HTMLLIElement) => filterContent.appendChild(value))
         rawHTML.append(filterContent)
         rawHTML.getElementsByClassName('filter_card_header_btn')[0]
@@ -42,6 +68,15 @@ export class FilterItem implements IFilterItem {
                 this.collapseFilter(filterContent.id)
             })
         return rawHTML
+    }
+
+    private addHeader(): HTMLDivElement {
+        this.headerFilter.innerHTML =
+            `<div class="filter_card_header">
+                <span class="filter_card_header_title">${this.filterTitle}</span>
+                <div id="filter_item_btn" class="filter_card_header_btn"></div>
+            </div>`
+        return this.headerFilter
     }
 
     private collapseFilter(collapseElementId: string): void {
